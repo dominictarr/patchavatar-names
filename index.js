125e9d7c4edba7c6b1013cd7310a63152022750e
Dominic Tarr <dominic.tarr@gmail.com>
2017-12-07 00:30:55 +1300

    update

---
+++ index.js
@@ -0,0 +1,43 @@
+var u = require('ssb-names/util')
+
+exports.needs = {
+  sbot: {
+    friends: { get: 'first' },
+    names: { getSignifier: 'first'}
+  },
+  identity: {
+    main: 'first'
+  }
+}
+
+exports.gives = {
+  avatar: {
+    image: true,
+    name: true
+  }
+}
+
+exports.create = function (api) {
+  var friends, names, wait = []
+  return {
+    avatar: {
+      image: function () { }, //fall through...
+      name: function (id) {
+        var span = document.createElement('span')
+        span.title = id
+        api.sbot.names.getSignifier(id, function (err, name) {
+          span.textContent = name
+        })
+        if(!span.textContent)
+          span.textContent = id
+        return span
+      }
+    }
+  }
+}
+
+
+
+
+
+
